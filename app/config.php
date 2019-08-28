81f987fffd8a01b7ca5f86c376206486d54f05c4
Hannan Miah <hannanhridoy@gmail.com>
2019-08-28 19:26:01 +0600

    Just changed some orientation and code stucture

--- app/config.php
+++ app/config.php
@@ -1,16 +1,16 @@
 <?php
 
 spl_autoload_register(function ($class) {
-    if (file_exists('classes/' . $class . '.php')) {
-        include_once 'classes/' . $class . '.php';
+    if (file_exists('Classes/' . $class . '.php')) {
+        include_once 'Classes/' . $class . '.php';
     } elseif (file_exists('app/' . $class . '.php')) {
         include_once 'app/' . $class . '.php';
-    } elseif (file_exists('controllers/' . $class . '.php')) {
-        include_once 'controllers/' . $class . '.php';
-    } elseif (file_exists('views/' . $class . '.php')) {
-        include_once 'views/' . $class . '.php';
-    } elseif (file_exists('models' . $class . '.php')) {
-        include_once 'models/' . $class . '.php';
+    } elseif (file_exists('Controllers/' . $class . '.php')) {
+        include_once 'Controllers/' . $class . '.php';
+    } elseif (file_exists('Views/' . $class . '.php')) {
+        include_once 'Views/' . $class . '.php';
+    } elseif (file_exists('Models' . $class . '.php')) {
+        include_once 'Models/' . $class . '.php';
     } else {
         die('Class ' . $class . ' not found!');
     }
